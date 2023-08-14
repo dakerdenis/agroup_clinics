88ee00c01da7674e0acee465f22ecf8ce6776001
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-08-14 13:58:49 +0400

    added

---
+++ admin/include/add.php
@@ -0,0 +1,63 @@
+
+<?php
+if($_GET['type'] == 'clinic'){
+    
+} else {
+    
+} 
+
+?>
+
+<div class="add_container">
+    <from class="add__element__form">
+        <div class="add__element__container">
+            <div class="add__element__block">
+                <div class="add__element__desc">
+                   Clinic's name
+                </div>
+                <div class="add__element__input">
+                    <input type="text" placeholder="add clinic's name">
+                </div>
+            </div>
+            <div class="add__element__block">
+                <div class="add__element__desc">
+                   Clinic's adress
+                </div>
+                <div class="add__element__input">
+                    <input type="text" placeholder="add clinic's adress">
+                </div>
+            </div>
+            <div class="add__element__block">
+                <div class="add__element__desc">
+                   Clinic's phone
+                </div>
+                <div class="add__element__input">
+                    <input type="text" placeholder="add clinic's phone">
+                </div>
+            </div>
+
+            <div class="add__element__block">
+                <div class="add__element__desc">
+                   Clinic's location X
+                </div>
+                <div class="add__element__input">
+                    <input type="text" placeholder="add clinic's location X">
+                </div>
+            </div>
+            <div class="add__element__block">
+                <div class="add__element__desc">
+                   Clinic's Location Y
+                </div>
+                <div class="add__element__input">
+                    <input type="text" placeholder="add clinic's Location Y">
+                </div>
+            </div>
+
+            <div class="add__element__button">
+                    <button name="submit" type="submit">
+                        Add Clinic
+                    </button>
+            </div>
+        </div>
+    </from>
+</div>
--- admin/include/header.php
+++ admin/include/header.php
@@ -5,10 +5,10 @@
             <a class="header__Logo"  href="./admin.php?page=data">
                 <img src="../style/Logo.svg" alt="">
             </a>
-            <a class="header__add_element" href="#">
+            <a class="header__add_element" href="./admin.php?page=add&type=clinic">
                 Добавить Клинику
             </a>
-            <a class="header__add_element" href="#">
+            <a class="header__add_element" href="./admin.php?page=add&type=aptek">
                 Добавить Аптеку
             </a>
 
